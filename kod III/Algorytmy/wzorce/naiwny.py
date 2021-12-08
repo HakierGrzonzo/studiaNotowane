07b0df6c09071cfae737eb8d6ca49e3932f29314
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2021-12-08 10:58:52 +0100

    sync

--- kod III/Algorytmy/wzorce/naiwny.py
+++ kod III/Algorytmy/wzorce/naiwny.py
@@ -1,7 +1,6 @@
 def naiwny(source:str, toFind: str) -> int:
     for i in range(0, len(source) - len(toFind)):
         for j in range(0, len(toFind)):
-            print(source[i + j], toFind[j], i, j)
             if source[i + j] != toFind[j]:
                 break
         else:
